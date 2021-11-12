b22f911910adae6847bda814c81481adf650770d
Bruno Mendes <mendessbruno@gmail.com>
2021-11-12 15:54:34 -0300

    Feat: logica logoff

--- frontEnd/pastelaria/src/components/nav.js
+++ frontEnd/pastelaria/src/components/nav.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
       redirectRoute:'',
     };
     // this.handleChanges = this.handleChanges.bind(this);
     // this.handleSubmit = this.handleSubmit.bind(this);
   }
-
+logoff = () => {
+  localStorage.clear();
+  this.setState({
+    redirectRoute: '/login',
+  })
+}
 render() {
   const { user, navButtons, changeNav } = this.props
+  const { redirectRoute } = this.state;
   return (
+    redirectRoute? <Redirect to={`${redirectRoute}`}/> :
     <nav className="bg-gray-800 flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
       <div className= 'text-gray-300 px-3 py-2 rounded-md text-sm font-medium'>{`Bem vindo ${user}`}</div>
       <div className="flex space-x-4">
@@ -27,7 +33,9 @@ render() {
           }) : <>Loading</>
         }
       </div>
-      <button className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>logoff</button>
+      <button className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'
+      onClick={(()=> this.logoff())}
+      >logoff</button>
     </nav>
   )}
 }
